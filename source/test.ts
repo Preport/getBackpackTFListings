a9361242e9956b1d51810b935ca1fa9b59b64994
Preport <erdemgezzz@gmail.com>
2021-05-20 01:12:39 +0300

    Update tests

--- source/test.ts
+++ source/test.ts
@@ -1,7 +1,7 @@
 import getListings from '.'
 const gl = new getListings();
 
-
+let logOne = true;
 let urls = [
     'https://backpack.tf/classifieds?quality=15&tradable=1&craftable=1&australium=-1&killstreak_tier=0',
     'https://backpack.tf/classifieds?quality=6&tradable=1&craftable=1&australium=-1&killstreak_tier=0&spell=Exorcism%2CVoices%20from%20Below%2CPumpkin%20Bombs%2CHalloween%20Fire',
@@ -15,6 +15,10 @@ async function t() {
     res.forEach((listings, i) => {
         ["buy", "sell"].forEach(intent => {
             listings[intent as "buy"].find(list => {
+                if (logOne) {
+                    console.log(list)
+                    logOne = false;
+                }
                 switch (i) {
                     case 0:
                         //checking paints || Item's with unusual effects are counted as "Decorated Weapon" as well so ignore them.
